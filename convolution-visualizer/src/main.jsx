f7a37642bed760b57bcadff9223605cce1abce41
Suyash Mudgal <suyashmudgal18@gmail.com>
2026-05-12 23:24:04 +0530

    feat: set up React Router with landing page at / and visualizer at /visualizer

--- convolution-visualizer/src/main.jsx
+++ convolution-visualizer/src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
+import './landing.css'
 import App from './App.jsx'
+import LandingPage from './components/LandingPage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/visualizer" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
